5ed839d787af2ef4dfc4c49b76710d763a70df90
shuang3322 <shuang0528@hotmail.com>
2018-05-23 15:30:19 +0800

    add doc

--- work4/open_file_test.py
+++ work4/open_file_test.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*
-with open('name.db','r',encoding='utf-8') as f:
+import os
+import sys
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(base_dir)
+with open('db/name.db','r',encoding='utf-8') as f:
     f1= f.readlines()
-    print(f1,type(f1))
+    for item in f1 :
+        print(item.split(",")[0:])
 
 ss = "aaa"
 ss.strip()
