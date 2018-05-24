3b957fd1306514f3ae348ee973b9bcce42b77f0f
shuang3322 <shuang0528@hotmail.com>
2018-05-24 14:01:56 +0800

    copy all

---
+++ Python开发14天集训营课件/第1~4章/chapter3-文件操作与函数/函数/嵌套函数.py
@@ -0,0 +1,14 @@
+#_*_coding:utf-8_*_
+
+name = "Alex"
+
+
+def change_name():
+    name = "Alex2"
+    def change_name2():
+        name = "Alex3"
+        print("第3层打印", name)
+    change_name2()  # 调用内层函数
+    print("第2层打印", name)
+change_name()
+print("最外层打印", name)
